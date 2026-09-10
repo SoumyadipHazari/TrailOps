ae2479281c74fa03f8317def04f76fc226281a48
SoumyadipHazari <166205459+SoumyadipHazari@users.noreply.github.com>
2026-09-10 19:09:36 +0530

    feat: logout button

---
+++ frontend/src/components/LogoutButton.vue
@@ -0,0 +1,33 @@
+<template>
+    <button
+        class="btn btn-danger"
+        @click="logout"
+    >
+        Logout
+    </button>
+</template>
+
+<script setup>
+import api from "../services/api";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+async function logout() {
+
+    try {
+
+        await api.post("/auth/logout");
+
+        router.push("/login");
+
+    }
+
+    catch (err) {
+
+        console.error(err);
+
+    }
+
+}
+</script>
